154b5f714807e11c5a9b307f15197c55c99a4f0c
Florian Koch <flkoch@dev.fkoch.ch>
2018-06-26 10:56:50 +0200

    updated paths to be absolute requires this folder to be clonde into home directory of user

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ rm -rd ~/.config/i3status
 mkdir ~/.config/i3
 mkdir ~/.config/i3status
 
-ln -s dotfiles/.bashrc ~/
-ln -s dotfiles/.bash_aliases ~/
-ln -s dotfiles/.config/i3/config ~/.config/i3/
-ln -s dotfiles/.config/i3status/config ~/.config/i3status/
+ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.bash_aliases ~/
+ln -s ~/dotfiles/.config/i3/config ~/.config/i3/
+ln -s ~/dotfiles/.config/i3status/config ~/.config/i3status/
