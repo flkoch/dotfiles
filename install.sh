04fba1c37aee9d847587021982f3be2274c5b7df
Florian Koch <flkoch@dev.fkoch.ch>
2018-06-26 10:50:13 +0200

    added i3 and i3status configuration

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 rm ~/.bashrc
 rm ~/.bash_aliases
+rm -rd ~/.config/i3
+rm -rd ~/.config/i3status
+mkdir ~/.config/i3
+mkdir ~/.config/i3status
 
 ln -s dotfiles/.bashrc ~/
 ln -s dotfiles/.bash_aliases ~/
+ln -s dotfiles/.config/i3/config ~/.config/i3/
+ln -s dotfiles/.config/i3status/config ~/.config/i3status/
