f450362bdda020a51db646d4f9e74cdea13eb762
Florian Koch <flkoch@T520L.home>
2018-08-14 19:32:20 +0200

    Added nvim configuration

---
+++ .config/nvim/init.vim
@@ -0,0 +1,18 @@
+" Plugins
+call plug#begin()
+Plug 'ctrlpvim/ctrlp.vim'
+Plug 'neomake/neomake'
+
+call plug#end()
+
+" Set linenumbers
+set number relativenumber
+" Make search more holpful
+set incsearch
+set hlsearch
+" Show cursor position
+set ruler
+" Set terminal's title
+set title
+" Default encoding
+set encoding=utf-8
--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# remove previous files and folders
 rm ~/.bashrc
 rm ~/.bash_aliases
-rm -rd ~/.config/i3
-rm -rd ~/.config/i3status
-mkdir ~/.config/i3
-mkdir ~/.config/i3status
+rm -rfd ~/.config/i3
+rm -rfd ~/.config/i3status
+rm -rfd ~/.config/nvim
+# Create all required folders
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/share/nvim/site/autoload
 
+# Create links for all configurations files
 ln -s ~/dotfiles/.bashrc ~/
 ln -s ~/dotfiles/.bash_aliases ~/
 ln -s ~/dotfiles/.config/i3/config ~/.config/i3/
 ln -s ~/dotfiles/.config/i3status/config ~/.config/i3status/
+ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/
+
+# Install vim-plug plugin manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
