82d66da253cea778acb2e124cca1f4c54e8a33b1
Florian Koch <flkoch@dev.fkoch.ch>
2019-12-26 14:16:20 +0100

    Simplified setup script

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+ 
+# Find all dot files, back them up if they exist and create a symlink to the new dot file
+for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
+	if [ -e ~/$file ]; then
+		mv -f ~/$file{,.bck}
+	fi
+	ln -s $PWD/$file ~/$file
+done
 
-# remove previous files and folders
-rm ~/.bashrc
-rm ~/.bash_aliases
-rm -rfd ~/.config/i3
-rm -rfd ~/.config/i3status
-rm -rfd ~/.config/nvim
-# Create all required folders
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/i3status
-mkdir -p ~/.config/nvim
-mkdir -p ~/.local/share/nvim/site/autoload
-
-# Create links for all configurations files
-ln -s ~/dotfiles/.bashrc ~/
-ln -s ~/dotfiles/.bash_aliases ~/
-ln -s ~/dotfiles/.config/i3/config ~/.config/i3/
-ln -s ~/dotfiles/.config/i3status/config ~/.config/i3status/
-ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/
+# For all folders in .config, back them up if they exist and create a symlink to the new folder
+for folder in $(ls .config/ ); do
+	if [ -d ~/.config/$folder ]; then
+		mv -f ~/.config/$folder{,.bck}
+	fi
+	mkdir ~/.config/$folder
+	for file in $(ls .config/$folder ); do
+		ln -s $PWD/.config/$folder/$file ~/.config/$folder/$file
+	done
+done
 
 # Install vim-plug plugin manager
+mkdir -p ~/.local/share/nvim/site/autoload
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installed"
